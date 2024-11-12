891e886eb4bb8c29db5951aace6e0bc54b0a9c5c
Jesse Mather <jmather@arista.com>
2024-11-12 15:17:10 -0800

    add gopls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,8 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls", "gopls" }
+local servers = {"html", "gopls", "pyright", "zls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
